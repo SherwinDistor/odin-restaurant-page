c059fb9c54972400edc73f1d1b04ba33664a65a5
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-01-06 15:04:13 -0600

    Add paragraph elements to content div

--- src/content.js
+++ src/content.js
@@ -7,7 +7,35 @@ const loadContent = () => {
     restaurantName.textContent = 'Welcome To Minervas';
     contentDiv.appendChild(restaurantName);
 
-    
+    // Create div with img inside
+    const restaurantImg = document.createElement('img');
+    const restaurantImgDiv = document.createElement('div');
+    restaurantImg.src = 'https://images.letgroup.com/OoYrbF6EFm/cmsimg_1672861375.jpg';
+    restaurantImg.alt = 'Inside Minervas restaurant';
+    restaurantImgDiv.appendChild(restaurantImg);
+    contentDiv.appendChild(restaurantImgDiv);
+
+    // Create div with three p elements inside
+    const restaurantParagraphDiv = document.createElement('div');
+
+    // Create the first paragraph
+    const paragraph1 = document.createElement('p');
+    paragraph1.textContent = 'Only the greatest dining experiences stand the tests of time and taste. Minervas has proudly served guests since 1977, from the heart of downtown Sioux Falls.';
+    restaurantParagraphDiv.appendChild(paragraph1);
+
+    // Create the second paragraph
+    const paragraph2 = document.createElement('p');
+    paragraph2.textContent = 'Located at the corner of Phillips Avenue and 11 Street, Minervas is known for its deliciously diverse, evolving menu.';
+    restaurantParagraphDiv.appendChild(paragraph2);
+
+    // Create the third paragraph
+    const paragraph3 = document.createElement('p');
+    paragraph3.textContent = 'Guests return for generations to savor our hand-cut, in-house aged steaks, fresh seafood, chops, classic pasta dishes, the freshest salads, and house-made desserts.';
+    restaurantParagraphDiv.appendChild(paragraph3);
+
+    // Append the div with all the paragraphs to the content div
+    contentDiv.appendChild(restaurantParagraphDiv);
+
 }
 
 export { loadContent };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './styles.css';
+import { loadContent } from './content';
 
 console.log('hello world');
+
+loadContent();
