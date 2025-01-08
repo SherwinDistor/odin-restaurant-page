df4d93a0566ebd3b6a2ccb96515a9b18aab5fe28
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-01-08 10:39:32 -0600

    Create main div, moving to own file

--- src/load-home.js
+++ src/load-home.js
@@ -2,10 +2,20 @@ const loadHome = () => {
     // Get div#content
     const contentDiv = document.getElementById('content');
 
+    // Create main div
+    const mainDiv = document.createElement('div');
+    mainDiv.id = 'main';
+    contentDiv.appendChild(mainDiv);
+
+    // Create div#home
+    const homeDiv = document.createElement('div');
+    homeDiv.id = 'home';
+    mainDiv.appendChild(homeDiv);
+
     // Create h1 element with restaurant name
     const restaurantName = document.createElement('h1');
     restaurantName.textContent = 'Welcome To Minervas';
-    contentDiv.appendChild(restaurantName);
+    homeDiv.appendChild(restaurantName);
 
     // Create div with img inside
     const restaurantImg = document.createElement('img');
@@ -13,7 +23,7 @@ const loadHome = () => {
     restaurantImg.src = 'https://images.letgroup.com/OoYrbF6EFm/cmsimg_1672861375.jpg';
     restaurantImg.alt = 'Inside Minervas restaurant';
     restaurantImgDiv.appendChild(restaurantImg);
-    contentDiv.appendChild(restaurantImgDiv);
+    homeDiv.appendChild(restaurantImgDiv);
 
     // Create div with three p elements inside
     const restaurantParagraphDiv = document.createElement('div');
@@ -21,20 +31,23 @@ const loadHome = () => {
     // Create the first paragraph
     const paragraph1 = document.createElement('p');
     paragraph1.textContent = 'Only the greatest dining experiences stand the tests of time and taste. Minervas has proudly served guests since 1977, from the heart of downtown Sioux Falls.';
+    paragraph1.classList.add('home-p');
     restaurantParagraphDiv.appendChild(paragraph1);
 
     // Create the second paragraph
     const paragraph2 = document.createElement('p');
     paragraph2.textContent = 'Located at the corner of Phillips Avenue and 11 Street, Minervas is known for its deliciously diverse, evolving menu.';
+    paragraph2.classList.add('home-p');
     restaurantParagraphDiv.appendChild(paragraph2);
 
     // Create the third paragraph
     const paragraph3 = document.createElement('p');
     paragraph3.textContent = 'Guests return for generations to savor our hand-cut, in-house aged steaks, fresh seafood, chops, classic pasta dishes, the freshest salads, and house-made desserts.';
+    paragraph3.classList.add('home-p');
     restaurantParagraphDiv.appendChild(paragraph3);
 
     // Append the div with all the paragraphs to the content div
-    contentDiv.appendChild(restaurantParagraphDiv);
+    homeDiv.appendChild(restaurantParagraphDiv);
 
 }
 
